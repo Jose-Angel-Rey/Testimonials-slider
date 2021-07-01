32477f7bd78519e4da218b7e7bc89b3cc4848ab4
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-07-01 11:05:43 -0400

    Update

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: "Susan Smith",
+    job: "Web developer",
+    img: "./assets/susan.webp",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "Anna Johnson",
+    job: "Web designer",
+    img: "./assets/anna.webp",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "Pamela Jones",
+    job: "Intern",
+    img: "./assets/pamela.webp",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "Karla anderson",
+    job: "The boss",
+    img: "./assets/karla.webp",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
